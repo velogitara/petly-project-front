4dd8424813cc384f035d9f6b0a6ec97bac4c99f1
Вадим Фатеев <fateevdanon@gmail.com>
2022-11-10 18:36:43 +0100

    html

---
+++ src/pages/OurFriendsPage/OurFriendsPage.jsx
@@ -0,0 +1,37 @@
+import sponsors from './sponsors.json';
+const OurFriendsPage = () => {
+  return (
+    <div>
+      <h2>OurFriendsPage</h2>
+      <ul>
+        {sponsors.map(el => {
+          const { title, url, addressUrl, imageUrl, address, workDays, phone, email } = el;
+          return (
+            <li>
+              <h3>
+                <a href={url}></a>
+                {title}
+              </h3>
+              <img src={imageUrl} alt={title + ' logo'} />
+              <p>TIME: </p>
+              <p>
+                Adress:
+                <a href={addressUrl}>{address}</a>
+              </p>
+              <p>
+                Email:{' '}
+                <a href={'mailto:' + (email || '---------------')}>{email || '---------------'}</a>
+              </p>
+              <p>Phone:</p>
+              <p>
+                <a href={'tel:' + (phone || '---------------')}>{phone || '---------------'}</a>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default OurFriendsPage;
