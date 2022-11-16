80471669e516410fb0893d833dc4df27fc2fb6d8
Вадим Фатеев <fateevdanon@gmail.com>
2022-11-16 19:49:10 +0100

    Update OurFriendsTitle.jsx

--- src/components/OurFriendsTitle/OurFriendsTitle.jsx
+++ src/components/OurFriendsTitle/OurFriendsTitle.jsx
@@ -2,7 +2,7 @@ import StyledComponents from './OurFriendsTitle.styled';
 const { Title } = StyledComponents;
 
 const OurFriendsTitle = () => {
-  return <Title>Our friend</Title>;
+  return <Title>Our friends</Title>;
 };
 
 export default OurFriendsTitle;
