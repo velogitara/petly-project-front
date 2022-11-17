ff898aa2373e5df3ce00c778a5712cdb8170674a
velogitara <93466688+velogitara@users.noreply.github.com>
2022-11-17 08:27:32 +0000

    Deploying to gh-pages from @ velogitara/petly-project-front@c94a52b13012c51046efa4f334e3839d635aa8a5 🚀

--- static/js/35.881c82d4.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkpetly_project_front=self.webpackChunkpetly_project_front||[]).push([[35],{2035:function(e,t,n){n.r(t),n.d(t,{default:function(){return u}});var r=n(3329),u=function(){return(0,r.jsx)("div",{children:"HomePage"})}}}]);
-//# sourceMappingURL=35.881c82d4.chunk.js.map
