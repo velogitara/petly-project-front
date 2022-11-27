7553e571d01acd1b0ffae99aa6899367dfa4bc84
Roman Bezuhlyi <bezuglijj7@gmail.com>
2022-11-27 17:38:37 +0200

    image@2x

--- src/components/HomeImg/HomeImg.jsx
+++ src/components/HomeImg/HomeImg.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 import { Image } from './HomeImg.styled';
 import imageDesktop from '../../assets/images/hero-624.png';
+import imageDesktopR from '../../assets/images/hero-624@2x.png';
 import imageTablet from '../../assets/images/hero-699.png';
+import imageTabletR from '../../assets/images/hero-699@2x.png';
 import imageMobile from '../../assets/images/hero-320.png';
+import imageMobileR from '../../assets/images/hero-320@2x.png';
 
 const HomeImg = () => {
   return (
     <Image>
-      <source srcSet={imageMobile} media="(max-width: 767px)" />
-      <source srcSet={imageTablet} media="(max-width: 1279px)" />
-      <source srcSet={imageDesktop} media="(min-width: 1280px)" />
+      <source srcSet={(imageMobileR, imageMobile)} media="(max-width: 767px)" type="image/png" />
+      <source srcSet={(imageTabletR, imageTablet)} media="(max-width: 1279px)" type="image/png" />
+      <source srcSet={(imageDesktopR, imageDesktop)} media="(min-width: 1280px)" type="image/png" />
       <img src={imageDesktop} alt="hero" />
     </Image>
   );
