b8db970296cf3421d57f3e74e5807b024a9e1633
unknown <velogitara@gmail.com>
2022-11-27 08:03:10 +0200

    add some

--- src/components/InputSearch/InputSearch.jsx
+++ src/components/InputSearch/InputSearch.jsx
@@ -1,12 +1,21 @@
 import React from 'react';
 import { InputStyled, BtnSearch, InputContainer } from './InputSearch.styled';
 import icons from '../../assets/icons/icons.svg';
+import { useListNoticesByQueryMutation } from '../../redux/notices';
 
 const InputSearch = ({ onSubmit }) => {
+  // useListNoticesByQueryQuery();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log(e.currentTarget.parentElement.elements['search'].value);
+  };
+
   return (
     <InputContainer id="searchForm">
       <InputStyled placeholder="Search" type="text" name="search" />
-      <BtnSearch onClick={(e) => onSubmit(e)} >
+      <BtnSearch onClick={handleSubmit}>
         <svg width={17.5} height={17.5}>
           <use href={`${icons}#icon-search`}></use>
         </svg>
